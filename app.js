5dae14aa6d8749637932ce01fd2a0d9ab48a42b9
Kevin Sidlacan <sidlacankevindale@gmail.com>
2021-05-28 22:33:59 +0800

    add routes files

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 4000
 
+// middlewares
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
 
+// routes
+app.use('/api/users', require('./routes/users'))
+app.use('/api/income', require('./routes/income'))
+app.use('/api/expenses', require('./routes/expenses'))
 
 app.listen( port ,()=>{
     console.log(`Server running on port ${port}`)
