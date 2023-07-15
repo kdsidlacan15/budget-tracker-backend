82ac125ab52fb3d3b89345daa2c4e577cc7318fd
Kevin Sidlacan <sidlacankevindale@gmail.com>
2023-07-16 01:06:13 +0800

    refactor income routes

---
+++ routes/controllers/incomeController.js
@@ -0,0 +1,72 @@
+const Income = require("../../models/Income");
+
+// Get All Income
+const getAllIncome = async (req, res, next) => {
+  const { _id } = req.user;
+  try {
+    const allIncome = await Income.find({ user: _id });
+    res.send(allIncome);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Create Income
+const addIncome = async (req, res, next) => {
+  const { _id } = req.user;
+  const { category, entry, value } = req.body;
+  try {
+    const income = await Income.create({
+      category,
+      user: _id,
+      entry,
+      value,
+    });
+    res.send(income);
+  } catch (error) {
+    next(error);
+  }
+};
+// Update Income
+const updateIncome = async (req, res, next) => {
+  try {
+    const updatedIncome = await Income.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        omitUndefined: true,
+      }
+    );
+    res.send(updatedIncome);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Delete Income
+const deleteIncome = async (req, res, next) => {
+  try {
+    const deletedIncome = await Income.findByIdAndDelete(req.params.id);
+    res.send(deletedIncome);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Admin View All Income of specific User
+const viewIncome = async (req, res, next) => {
+  try {
+    const userIncome = await Income.find({ user: req.params.id });
+    res.send(userIncome);
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports = {
+  getAllIncome,
+  addIncome,
+  updateIncome,
+  deleteIncome,
+  viewIncome,
+};
--- routes/income.js
+++ routes/income.js
@@ -1,71 +1,27 @@
 const router = require("express").Router();
-const Income = require("./../models/Income");
 const { verifyUser } = require("./../utils");
 const { verifyAdmin } = require("./../utils");
+const {
+  getAllIncome,
+  addIncome,
+  updateIncome,
+  deleteIncome,
+  viewIncome,
+} = require("./controllers/incomeController");
 
 //Add Income
-router.post("/", verifyUser, (req, res, next) => {
-  // let description = req.body.entry.map(entry => entry.description)
-  // console.log(description)
-  // let value = req.body.entry.map(entry => entry.value)
-  // console.log(value)
-
-  // let newDescription = Object.assign({}, description)
-  // console.log(newDescription)
-  // let newValue= Object.assign({}, value)
-  // console.log(newValue)
-
-  Income.create({
-    category: req.body.category,
-    user: req.user.id,
-    entry: req.body.entry,
-    value: req.body.value,
-  })
-    .then((income) => {
-      res.send(income);
-    })
-    .catch(next);
-});
-
-module.exports = router;
+router.post("/", verifyUser, addIncome);
 
 //Get All User Income
-router.get("/", verifyUser, (req, res, next) => {
-  const { _id } = req.user;
-  Income.find({ user: _id })
-    .then((income) => {
-      res.send(income);
-    })
-    .catch(next);
-});
+router.get("/", verifyUser, getAllIncome);
 
 //Update Income Entry/Category
-router.put("/:id", verifyUser, (req, res, next) => {
-  Income.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    omitUndefined: true,
-  })
-    .then((income) => {
-      res.send(income);
-    })
-    .catch(next);
-});
+router.put("/:id", verifyUser, updateIncome);
 
 //Delete Income Entry/Category
-
-router.delete("/:id", verifyUser, (req, res, next) => {
-  Income.findByIdAndDelete(req.params.id)
-    .then((income) => {
-      res.send(income);
-    })
-    .catch(next);
-});
+router.delete("/:id", verifyUser, deleteIncome);
 
 //Admin View income of Specific User
-router.get("/:id", verifyUser, verifyAdmin, (req, res, next) => {
-  Income.find({ user: req.params.id })
-    .then((income) => {
-      res.send(income);
-    })
-    .catch(next);
-});
+router.get("/:id", verifyUser, verifyAdmin, viewIncome);
+
+module.exports = router;
